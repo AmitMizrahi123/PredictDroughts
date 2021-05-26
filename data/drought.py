a8015943246507775fca0fd49954c052a267e29c
AmitMizrahi123 <amitmizrahi231055@gmail.com>
2021-05-26 10:23:31 +0300

    Working on data

---
+++ data/drought.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from selenium import webdriver
+import requests
+import time
+from bs4 import BeautifulSoup
+
+
+class Droughts:
+    states = None
+    url = None
+    iterState = None
+
+    def __init__(self, url):
+        self.url = url
+        self.setUp(self.url)
+
+    def __del__(self):
+        self.driver.quit()
+
+    def setUp(self, url):
+        self.driver = webdriver.Firefox(executable_path="./geckodriver")
+        self.driver.get(self.url)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+        time.sleep(1)
+        self.driver.find_element_by_id("atype_chosen").click()
+        self.driver.find_element_by_xpath("//li[@class='active-result' and text()='State']").click()
+        self.driver.find_element_by_name("datatabl_length").click()
+        self.driver.find_element_by_xpath("//select[@name='datatabl_length']/option[text()='All']").click()
+        selectElement = self.driver.find_element_by_id("asel")
+        self.states = [elem.get_attribute("textContent") for elem in selectElement.find_elements_by_tag_name("option")]
+    
+    def changeState(self):
+        dropDownSelect = self.driver.find_element_by_id("asel_chosen")
+        dropDownSelect.click()
+        dropDownUl = dropDownSelect.find_element_by_class_name("chosen-results")
+        dropDownLis = dropDownUl.find_elements_by_class_name('active-result')
+        dropDownLis[self.iterState].click()
+
+    def getData(self):
+        self.iterState = 0
+        data = []
+        for state in self.states:
+            self.changeState()
+            data.append(self.getDataForState())
+            self.iterState += 1
+        return data
+
+    def getDataForState(self):
+        html = self.driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        div = soup.select_one("#datatabl")
+        table = pd.read_html(str(div))[0]
+        table.insert(0, 'State', self.states[self.iterState], True)
+        return table
+
+dataframe = pd.DataFrame()
+droughts = Droughts(r'https://droughtmonitor.unl.edu/DmData/DataTables.aspx')
+dfList = droughts.getData()
+for df in dfList:
+    dataframe = dataframe.append(df)
+dataframe.reset_index(drop=True, inplace=True)
+
+dataframe.to_csv('files/drought.csv')
---
+++ data/meteorological.py
@@ -0,0 +1,82 @@
+import os, json, urllib, requests, webbrowser
+from bs4 import BeautifulSoup
+import pandas as pd
+from tqdm import tqdm
+
+output = "JSON"
+
+def getLonAndLat():
+    url = r'https://developers.google.com/public-data/docs/canonical/states_csv'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    div = soup.find('table')
+    table = pd.read_html(str(div))[0]
+    table.sort_values('name', ascending=True, inplace=True)
+    return table
+
+df = getLonAndLat()
+lon = df['longitude'].to_list()
+lat = df['latitude'].to_list()
+locations = list(zip(lat, lon))
+# locations = [(32.318231, -86.902298)]
+
+output_folder = r''
+base_url = r"https://power.larc.nasa.gov/cgi-bin/v1/DataAccess.py?request=execute&identifier=SinglePoint&tempAverage=DAILY&parameters=WS10M_MIN,QV2M,T2M_RANGE,WS10M,T2M,WS50M_MIN,T2M_MAX,WS50M,TS,WS50M_RANGE,WS50M_MAX,WS10M_MAX,WS10M_RANGE,PS,T2MDEW,T2M_MIN,T2MWET,PRECTOT&startDate=20000104&endDate=20210518&lat={latitude}&lon={longitude}&outputList={output}&userCommunity=SSE"
+
+dfList = []
+
+for latitude, longitude in tqdm(locations):
+    api_request_url = base_url.format(longitude=longitude, latitude=latitude, output=output.upper())
+    json_response = json.loads(requests.get(api_request_url).content)
+    TIME = [key for key in json_response['features'][0]['properties']['parameter']['PRECTOT'].keys()]
+    WS10M_MIN = [value for value in json_response['features'][0]['properties']['parameter']['WS10M_MIN'].values()]
+    QV2M = [value for value in json_response['features'][0]['properties']['parameter']['QV2M'].values()]
+    T2M_RANGE = [value for value in json_response['features'][0]['properties']['parameter']['T2M_RANGE'].values()]
+    WS10M = [value for value in json_response['features'][0]['properties']['parameter']['WS10M'].values()]
+    T2M = [value for value in json_response['features'][0]['properties']['parameter']['T2M'].values()]
+    WS50M_MIN = [value for value in json_response['features'][0]['properties']['parameter']['WS50M_MIN'].values()]
+    T2M_MAX = [value for value in json_response['features'][0]['properties']['parameter']['T2M_MAX'].values()]
+    WS50M = [value for value in json_response['features'][0]['properties']['parameter']['WS50M'].values()]
+    TS = [value for value in json_response['features'][0]['properties']['parameter']['TS'].values()]
+    WS50M_RANGE = [value for value in json_response['features'][0]['properties']['parameter']['WS50M_RANGE'].values()]
+    WS50M_MAX = [value for value in json_response['features'][0]['properties']['parameter']['WS50M_MAX'].values()]
+    WS10M_MAX = [value for value in json_response['features'][0]['properties']['parameter']['WS10M_MAX'].values()]
+    WS10M_RANGE = [value for value in json_response['features'][0]['properties']['parameter']['WS10M_RANGE'].values()]
+    PS = [value for value in json_response['features'][0]['properties']['parameter']['PS'].values()]
+    T2MDEW = [value for value in json_response['features'][0]['properties']['parameter']['T2MDEW'].values()]
+    T2M_MIN = [value for value in json_response['features'][0]['properties']['parameter']['T2M_MIN'].values()]
+    T2MWET = [value for value in json_response['features'][0]['properties']['parameter']['T2MWET'].values()]
+    PRECTOT = [value for value in json_response['features'][0]['properties']['parameter']['PRECTOT'].values()]
+
+    data = {
+        'week': TIME,
+        'PRECTOT': PRECTOT,
+        'WS10M_MIN': WS10M_MIN,
+        'QV2M': QV2M,
+        'T2M_RANGE': T2M_RANGE,
+        'WS10M': WS10M,
+        'T2M': T2M,
+        'WS50M_MIN': WS50M_MIN,
+        'T2M_MAX': T2M_MAX,
+        'WS50M': WS50M,
+        'TS': TS,
+        'WS50M_RANGE': WS50M_RANGE,
+        'WS50M_MAX': WS50M_MAX,
+        'WS10M_MAX': WS10M_MAX,
+        'WS10M_RANGE': WS10M_RANGE,
+        'PS': PS,
+        'T2MDEW': T2MDEW,
+        'T2M_MIN': T2M_MIN,
+        'T2MWET': T2MWET,
+        'PRECTOT': PRECTOT
+    }
+
+    df = pd.DataFrame(data)
+    dfList.append(df)
+
+dataframe = pd.DataFrame()
+for df in dfList:
+    dataframe = dataframe.append(df)
+dataframe.reset_index(drop=True, inplace=True)
+
+dataframe.to_csv('files/meteorological.csv')
